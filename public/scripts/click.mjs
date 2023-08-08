a69904cb23328a408b765266912828e831989204
Boyszy <boldizsarbanfia@Boyszys-Air.lan>
2023-08-08 15:26:48 +0100

    sdf

--- public/scripts/click.mjs
+++ public/scripts/click.mjs
@@ -3,3 +3,8 @@ const popup = document.querySelector('.popupP');
 function toggle() {
     popup.classList.toggle('hidden');
 }
+
+const popup2 = document.querySelector('.popupP2');
+function toggle2() {
+    popup2.classList.toggle('hidden');
+}
